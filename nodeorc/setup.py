d7bffdb02f4c44d1edd05c589298ef4a58510ab2
hcwinsemius <h.c.winsemius@gmail.com>
2023-07-31 17:49:01 +0200

    fix #6

---
+++ nodeorc/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
+here = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    name="nodeopenrivercam",
+    description="nodeopenrivercam (nodeorc) is a node processor shell around pyOpenRiverCam",
+    version="0.1.0",
+    url="https://github.com/localdevices/nodeorc",
+    author="Hessel Winsemius",
+    author_email="winsemius@rainbowsensing.com",
+    packages=find_packages(),
+    package_dir={"nodeorc": "nodeorc"},
+    test_suite="tests",
+    python_requires=">=3.9",
+    install_requires=[
+        "requests",
+        "pika",
+        "boto3",
+        "ibm-cos-sdk",
+        "pyopenrivercam",
+        "pydantic",
+    ],
+    extras_require={
+        "dev": ["pytest", "pytest-cov", "pytest-lazy-fixtures"],
+        "optional": [],
+    },
+    entry_points={
+        "console_scripts": [
+            "nodeorc = nodeorc.main:main"
+        ]
+    },
+
+    include_package_data=True,
+    license="AGPLv3",
+    zip_safe=False,
+    classifiers=[
+        # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Hydrology",
+        "Topic :: Scientific/Engineering :: Image Processing",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    keywords="hydrology, hydrometry, river-flow, pyorc, nodeorc",
+)
