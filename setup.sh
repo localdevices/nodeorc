484df46f75231ceafd25905664d8befbe8ba9bfd
hcwinsemius <h.c.winsemius@gmail.com>
2024-02-24 17:40:45 +0100

    setup reads callback url from user inputs #47

--- setup.sh
+++ setup.sh
@@ -30,13 +30,13 @@ install_dependencies () {
     then
         echo "CentOS system detected, running yum"
 	sudo yum -y update
-	sudo yum install ffmpeg libsm6 libxext6 libgl1 python3-venv libgdal-dev -y
+	sudo yum install ffmpeg libsm6 libxext6 libgl1 python3-venv libgdal-dev jq -y
     elif [[ `hostnamectl` =~ .*"Ubuntu".* ]] || [[ `hostnamectl` =~ .*"pop-os".* ]] || [[ `hostnamectl` =~ .*"Mint".* ]] || [[ `hostnamectl` =~ .*"Debian".* ]];
     then
         echo "Ubuntu-like system detected, trying apt"
         sudo apt -y update
         sudo apt -y upgrade
-        sudo apt install ffmpeg libsm6 libxext6 libgl1 python3-venv -y
+        sudo apt install ffmpeg libsm6 libxext6 libgl1 python3-venv jq -y
     else
         echo "System unknown I can't help you with updating"
     fi
@@ -56,6 +56,7 @@ install_nodeorc () {
 }
 
 install_service () {
+    export SCRIPT_FOLDER=$PWD/$(dirname $0)
     echo '################################'
     echo 'INSTALLING SYSTEMD SERVICE '
     echo '################################'
@@ -69,30 +70,68 @@ install_service () {
         * ) echo "No changes in device name made";;
     esac
     echo "Your device name is $DEVICE_NAME"
-    cat > nodeorc.service <<EOF
-[Unit]
-Description=NodeOpenRiverCam operational edge or cloud compute instance
-After=network.target
 
-[Service]
-# User=${USER}
-WorkingDirectory=${PWD}
-Environment="PATH=${HOME}/venv/nodeorc/bin"
-ExecStart=${HOME}/venv/nodeorc/bin/nodeorc start --storage local --listen local
-Restart=always
+#    read -p "Provide an ip-address or remote hostname (without /api/) where your LiveOpenRiverCam is available: " URL
+#    # remove trailing slah if any
+#    if [ "${URL: -1}" == "/" ] ; then export URL=${URL:0:-1} ; fi
+#    read -p "Provide your email login to ${URL}: " LOGIN
+#    read -s -p "Provide your password to ${URL}: " PASSWD
+    export URL="https://openrivercam.com"
+    export LOGIN="winsemius@rainbowsensing.com"
+    export PASSWD='^7JF!k93oqf$2EAl'
+    export data="{\"email\": \"${LOGIN}\", \"password\": \"${PASSWD}\"}"
+    export TOKEN=$(curl --silent --max-time 5 --location "${URL}/api/token/" --header 'Content-Type: application/json' --data-raw "$data")
 
-[Install]
-WantedBy=multi-user.target
-EOF
+#    export TOKEN=$(curl --silent --max-time 5 --location "${URL}/api/token/" --header 'Content-Type: application/json' --data-raw "{
+#        \"email\": \"${LOGIN}\",
+#        \"password\": \"${PASSWD}\"
+#    }")
+    if [[ $TOKEN = *"No active account"* ]]
+    then
+        echo "No active account found with provided credentials on ${URL}. Please contact your system administrator."
+        exit 1
+    fi
+    if [ "$TOKEN" = "" ]
+    then
+        echo "Host name ${URL} does not exist. Please contact your system administrator."
+        exit 1
+    fi
+    if [[ $TOKEN != *"access"* ]]
+    then
+        echo "${URL} seems to point to a OpenRiverCam server, but not to the right end point. Ensure you are not referring to any end points."
+        exit 1
+    fi
+    export ACCESS=$(echo $TOKEN | grep -o '"access":"[^"]*' | grep -o '[^"]*$')
+    export REFRESH=$(echo $TOKEN | grep -o '"refresh":"[^"]*' | grep -o '[^"]*$')
+    export CONFIG_TEMPLATE="${SCRIPT_FOLDER}/settings/config.json"
+    export CONFIG_NEW="${SCRIPT_FOLDER}/settings/config_device.json"
+    jq ".callback_url.url=\"${URL}\" | .callback_url.token_refresh=\"${REFRESH}\" | .callback_url.token_access=\"${ACCESS}\"" ${CONFIG_TEMPLATE} > ${CONFIG_NEW}
 
-echo 'moving systemd files to /etc/systemd/system'
-    sudo mv nodeorc.service /etc/systemd/system/
-    # ensuring credentials are set correctly
-    sudo chmod 644 /etc/systemd/system/nodeorc.service
-    echo 'starting and enabling the pyorc service with systemd'
-    sudo systemctl daemon-reload
-    sudo systemctl start nodeorc.service
-    sudo systemctl enable nodeorc.service
+
+#    cat > nodeorc.service <<EOF
+#[Unit]
+#Description=NodeOpenRiverCam operational edge or cloud compute instance
+#After=network.target
+#
+#[Service]
+## User=${USER}
+#WorkingDirectory=${PWD}
+#Environment="PATH=${HOME}/venv/nodeorc/bin"
+#ExecStart=${HOME}/venv/nodeorc/bin/nodeorc start --storage local --listen local
+#Restart=always
+#
+#[Install]
+#WantedBy=multi-user.target
+#EOF
+#
+#echo 'moving systemd files to /etc/systemd/system'
+#    sudo mv nodeorc.service /etc/systemd/system/
+#    # ensuring credentials are set correctly
+#    sudo chmod 644 /etc/systemd/system/nodeorc.service
+#    echo 'starting and enabling the pyorc service with systemd'
+#    sudo systemctl daemon-reload
+#    sudo systemctl start nodeorc.service
+#    sudo systemctl enable nodeorc.service
 }
 
 main() {
