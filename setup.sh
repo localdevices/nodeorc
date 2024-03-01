06d5a71fa77c4fbb0637b51aa72fa61bc877c792
hcwinsemius <h.c.winsemius@gmail.com>
2024-03-01 09:35:15 +0100

    fix #47

--- setup.sh
+++ setup.sh
@@ -56,7 +56,12 @@ install_nodeorc () {
 }
 
 install_service () {
+    # export variables for some static files below
     export SCRIPT_FOLDER=$PWD/$(dirname $0)
+    export CONFIG_TEMPLATE="${SCRIPT_FOLDER}/settings/config_template.json"
+    export CONFIG_NEW="${SCRIPT_FOLDER}/settings/config_device.json"
+    export CONFIG_DBASE=${HOME}/.nodeorc/nodeorc_config.db
+
     echo "========================================================================"
     echo 'INSTALLING SYSTEMD SERVICE '
     echo "========================================================================"
@@ -73,6 +78,7 @@ install_service () {
         deactivate
         exit 1
     fi
+    deactivate
     echo 'NodeORC is detected and installed. Continue to install service ...'
     echo ''
     echo 'USER INPUT'
@@ -81,48 +87,15 @@ install_service () {
     echo ''
     echo 'The name of your device must be unique and well recognizable in the LiveORC front-end.'
     DEVICE_NAME="$(hostname)"
-#    echo "The current name of your device is ${DEVICE_NAME}."
-#    read -p "Do you want to change this? [Y/n]" YN
-#    case $YN in
-#        [Yy]* ) read -e -p "Enter your new device name: " DEVICE_NAME;;
-#        * ) echo "No changes in device name made";;
-#    esac
-#    echo "Your device name will be $DEVICE_NAME"
-#
-##    read -p "Provide an ip-address or remote hostname (without /api/) where your LiveOpenRiverCam is available: " URL
-##    # remove trailing slah if any
-##    if [ "${URL: -1}" == "/" ] ; then export URL=${URL:0:-1} ; fi
-##    read -p "Provide your email login to ${URL}: " LOGIN
-##    read -s -p "Provide your password to ${URL}: " PASSWD
-    export URL="https://openrivercam.com"
-    export LOGIN="winsemius@rainbowsensing.com"
-    export PASSWD='^7JF!k93oqf$2EAl'
-#    export data="{\"email\": \"${LOGIN}\", \"password\": \"${PASSWD}\"}"
-#    export TOKEN=$(curl --silent --max-time 5 --location "${URL}/api/token/" --header 'Content-Type: application/json' --data-raw "$data")
-#
-##    export TOKEN=$(curl --silent --max-time 5 --location "${URL}/api/token/" --header 'Content-Type: application/json' --data-raw "{
-##        \"email\": \"${LOGIN}\",
-##        \"password\": \"${PASSWD}\"
-##    }")
-#    if [[ $TOKEN = *"No active account"* ]]
-#    then
-#        echo "No active account found with provided credentials on ${URL}. Please contact your system administrator."
-#        exit 1
-#    fi
-#    if [ "$TOKEN" = "" ]
-#    then
-#        echo "Host name ${URL} does not exist. Please contact your system administrator or check if you are connected to internet."
-#        exit 1
-#    fi
-#    if [[ $TOKEN != *"access"* ]]
-#    then
-#        echo "${URL} seems to point to a OpenRiverCam server, but not to the right end point. Ensure you are not referring to any end points."
-#        exit 1
-#    fi
-#    export ACCESS=$(echo $TOKEN | grep -o '"access":"[^"]*' | grep -o '[^"]*$')
-#    export REFRESH=$(echo $TOKEN | grep -o '"refresh":"[^"]*' | grep -o '[^"]*$')
-#    export CONFIG_TEMPLATE="${SCRIPT_FOLDER}/settings/config.json"
-#    export CONFIG_NEW="${SCRIPT_FOLDER}/settings/config_device.json"
+    echo "The current name of your device is ${DEVICE_NAME}."
+    read -p "Do you want to change this? [Y/n]" CHANGE_DEVICE
+    case $CHANGE_DEVICE in
+        [Yy]* ) read -e -p "Enter your new device name: " DEVICE_NAME; export CHANGE_DEVICE="Yes";;
+        * ) echo "No changes in device name made";export CHANGE_DEVICE="No";;
+    esac
+    echo "Your device name will be $DEVICE_NAME"
+    # collect all information on LiveORC
+    get_liveorc_data;
     # ask for info on storage options
     echo ''
     echo 'For portable devices runnbing from SD-cards such as Raspberry-Pis we highly recommend to use an external device'
@@ -162,48 +135,13 @@ install_service () {
     then
         setup_usb_mount;
     fi
-    echo "Setting hostname to ${DEVICE_NAME}"
-    hostnamectl set-hostname $DEVICE_NAME
-
-    echo "Setting up configuration with LiveORC connection to ${URL}..."
-    jq ".callback_url.url=\"${URL}\" | .callback_url.token_refresh=\"${REFRESH}\" | .callback_url.token_access=\"${ACCESS}\"" ${CONFIG_TEMPLATE} > ${CONFIG_NEW}
-
-    # we now have a solid configuration file, now initialize the database, and upload this to the database
-    export config_dbase=${HOME}/.nodeorc/nodeorc_config.db
-    if [[ -f ${config_dbase} ]]
+    if [[ $CHANGE_DEVICE == "Yes" ]]
     then
-        echo "Removing old database file ${config_dbase}"
-        rm ${config_dbase}
+        echo "Setting hostname to ${DEVICE_NAME}"
+        hostnamectl set-hostname $DEVICE_NAME
     fi
-    echo "Uploading configuration to a fresh database"
-    nodeorc upload-config ${CONFIG_NEW}
-    # deactivate python environment
-    deactivate
-    echo "Preparing nodeorc.service file for user ${USER}"
-    cat > nodeorc.service <<EOF
-[Unit]
-Description=NodeOpenRiverCam operational edge or cloud compute instance
-After=network.target
-
-[Service]
-# User=${USER}
-WorkingDirectory=${PWD}
-Environment="PATH=${HOME}/venv/nodeorc/bin"
-ExecStart=${HOME}/venv/nodeorc/bin/nodeorc start --storage local --listen local
-Restart=always
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-    echo 'moving systemd files to /etc/systemd/system'
-    sudo mv nodeorc.service /etc/systemd/system/
-    # ensuring credentials are set correctly
-    sudo chmod 644 /etc/systemd/system/nodeorc.service
-    echo 'starting and enabling the pyorc service with systemd'
-    sudo systemctl daemon-reload
-    sudo systemctl start nodeorc.service
-    sudo systemctl enable nodeorc.service
+    setup_nodeorc_config;
+    setup_service_files;
     echo "You are all set, no need to reboot. To check out live what is going on in the back end you may always type:"
     echo ""
     echo "sudo journalctl -u nodeorc.service -f"
@@ -225,6 +163,41 @@ main() {
                                                  install_nodeorc                 && \
                                                  install_service                 ;fi
     done
+}
+
+get_liveorc_data(){
+    read -p "Provide an ip-address or remote hostname (without /api/) where your LiveOpenRiverCam is available: " URL
+    # remove trailing slash if any
+    if [ "${URL: -1}" == "/" ] ; then export URL=${URL:0:-1} ; fi
+    read -p "Provide your email login to ${URL}: " LOGIN
+    read -s -p "Provide your password to ${URL}: " PASSWD
+#    export URL="https://openrivercam.com"
+#    export LOGIN="winsemius@rainbowsensing.com"
+#    export PASSWD='^7JF!k93oqf$2EAl'
+    export data="{\"email\": \"${LOGIN}\", \"password\": \"${PASSWD}\"}"
+    export TOKEN=$(curl --silent --max-time 5 --location "${URL}/api/token/" --header 'Content-Type: application/json' --data-raw "$data")
+
+#    export TOKEN=$(curl --silent --max-time 5 --location "${URL}/api/token/" --header 'Content-Type: application/json' --data-raw "{
+#        \"email\": \"${LOGIN}\",
+#        \"password\": \"${PASSWD}\"
+#    }")
+    if [[ $TOKEN = *"No active account"* ]]
+    then
+        echo "No active account found with provided credentials on ${URL}. Please contact your system administrator."
+        exit 1
+    fi
+    if [ "$TOKEN" = "" ]
+    then
+        echo "Host name ${URL} does not exist. Please contact your system administrator or check if you are connected to internet."
+        exit 1
+    fi
+    if [[ $TOKEN != *"access"* ]]
+    then
+        echo "${URL} seems to point to a OpenRiverCam server, but not to the right end point. Ensure you are not referring to any end points."
+        exit 1
+    fi
+    export ACCESS=$(echo $TOKEN | grep -o '"access":"[^"]*' | grep -o '[^"]*$')
+    export REFRESH=$(echo $TOKEN | grep -o '"refresh":"[^"]*' | grep -o '[^"]*$')
 }
 
 setup_usb_mount() {
@@ -260,6 +233,52 @@ setup_usb_mount() {
     fi
 
 }
+setup_nodeorc_config(){
+    source ${HOME}/venv/nodeorc/bin/activate
+
+    echo "Setting up configuration with LiveORC connection to ${URL}..."
+    jq ".callback_url.url=\"${URL}\" | .callback_url.token_refresh=\"${REFRESH}\" | .callback_url.token_access=\"${ACCESS}\"" ${CONFIG_TEMPLATE} > ${CONFIG_NEW}
+
+    # we now have a solid configuration file, now initialize the database, and upload this to the database
+    if [[ -f ${CONFIG_DBASE} ]]
+    then
+        echo "Removing old database file ${CONFIG_DBASE}"
+        rm ${CONFIG_DBASE}
+    fi
+    echo "Uploading configuration to a fresh database"
+    nodeorc upload-config ${CONFIG_NEW}
+    # deactivate python environment
+    deactivate
+}
+
+setup_service_files(){
+    echo "Preparing nodeorc.service file for user ${USER}"
+    cat > nodeorc.service <<EOF
+[Unit]
+Description=NodeOpenRiverCam operational edge or cloud compute instance
+After=network.target
+
+[Service]
+# User=${USER}
+WorkingDirectory=${PWD}
+Environment="PATH=${HOME}/venv/nodeorc/bin"
+ExecStart=${HOME}/venv/nodeorc/bin/nodeorc start --storage local --listen local
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+    echo 'moving systemd files to /etc/systemd/system'
+    sudo mv nodeorc.service /etc/systemd/system/
+    # ensuring credentials are set correctly
+    sudo chmod 644 /etc/systemd/system/nodeorc.service
+    echo 'starting and enabling the pyorc service with systemd'
+    sudo systemctl daemon-reload
+    sudo systemctl start nodeorc.service
+    sudo systemctl enable nodeorc.service
+}
+
 logo() {
 
     echo '
